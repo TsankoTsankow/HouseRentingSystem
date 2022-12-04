693b2d3dbd5e6b7ae20cee27203de7402c67b58a
Tsanko Tsankov <ts.tsankow@gmail.com>
2022-12-04 11:01:21 +0200

    WebApi Added

---
+++ HouseRentingSystem.Core/Services/StatisticsService.cs
@@ -0,0 +1,37 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Statistics;
+using HouseRentingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly ApplicationDbContext context;
+
+        public StatisticsService(ApplicationDbContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<StatisticsServiceModel> Total()
+        {
+            int totalHouses = await context.Houses
+                .CountAsync(h => h.IsActive);
+            int rentedHouses = await context.Houses
+                .CountAsync(h => h.IsActive && h.RenterId != null);
+
+            return new StatisticsServiceModel()
+            {
+                TotalHouses = totalHouses,
+                TotalRents = rentedHouses
+            };
+
+        }
+    }
+}
